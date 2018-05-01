e6c6b4db538bb57efd8bb2238038ba2175d2c020
gatasa <gabriel.taibo@gmail.com>
2018-05-01 16:37:56 +0200

    fixed some issues with password encoder

--- springSecurity/src/main/java/es/alfa/centauri/springSecurity/config/SecurityConfig.java
+++ springSecurity/src/main/java/es/alfa/centauri/springSecurity/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableAutoConfiguration
@@ -29,5 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
 				.permitAll();
 		http.exceptionHandling().accessDeniedPage("/403");
+	}
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+	return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 }
