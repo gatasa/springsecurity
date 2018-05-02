222a7d1a7f74ba3fc54a65ed1e3fe3165f2f8146
gatasa <gabriel.taibo@gmail.com>
2018-05-02 14:23:43 +0200

    adding mongo

---
+++ springSecurity/src/main/java/es/alfa/centauri/springSecurity/model/User.java
@@ -0,0 +1,25 @@
+package es.alfa.centauri.springSecurity.model;
+
+public class User {
+private String name;
+private String genre;
+private int age;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getGenre() {
+	return genre;
+}
+public void setGenre(String genre) {
+	this.genre = genre;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+}
